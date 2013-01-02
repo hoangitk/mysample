6b482e6c2b8527aa26a3af843f6866c78f16581f
hoangitk_cp <SND\hoangitk_cp>
2013-01-02 11:17:43 +0000

    Init

---
+++ TimeSheetRender.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace TimeSheetControl
+{
+    public class TimeSheetRender
+    {
+        public static Color GetColor(TimeSheetCatalog tsType)
+        {
+            switch (tsType)
+            {
+                case TimeSheetCatalog.WorkingDay:
+                    return Color.FromArgb(255, 255, 255);
+
+                case TimeSheetCatalog.Holiday:
+                    return Color.FromArgb(252, 213, 180);
+
+                case TimeSheetCatalog.WeekendOff:
+                    return Color.FromArgb(255, 190, 0);
+
+                case TimeSheetCatalog.WeekendOffHalf:
+                    return Color.FromArgb(178, 161, 199);
+
+                case TimeSheetCatalog.Leave:
+                    return Color.FromArgb(0, 112, 192);
+
+                case TimeSheetCatalog.BusinessTrip:
+                    return Color.FromArgb(165, 165, 165);
+
+                case TimeSheetCatalog.Overtime:
+                    return Color.FromArgb(192, 0, 0);
+
+                case TimeSheetCatalog.Shift:
+                    return Color.FromArgb(182, 221, 232);
+
+                default:
+                    throw new Exception("Invalid value for TimeSheetType");
+            }
+        }
+
+
+
+        public static Color GetColor(TimeSheetStatus tsStatus)
+        {
+            switch (tsStatus)
+            {
+                case TimeSheetControl.TimeSheetStatus.InvalidTS:
+                    return Color.FromArgb(182, 221, 232);
+
+                case TimeSheetControl.TimeSheetStatus.ValidTS:
+                    return Color.FromArgb(182, 221, 232);
+
+                case TimeSheetControl.TimeSheetStatus.UnApprovedOT:
+                    return Color.FromArgb(182, 221, 232);
+
+                case TimeSheetControl.TimeSheetStatus.ApprovedOT:
+                    return Color.FromArgb(182, 221, 232);
+
+                case TimeSheetControl.TimeSheetStatus.ApprovedLeave:
+                    return Color.FromArgb(182, 221, 232);
+
+                case TimeSheetControl.TimeSheetStatus.Locked:
+                    return Color.FromArgb(182, 221, 232);
+                default:
+                    throw new Exception("Invalid value for TimeSheetStatus");
+            }
+        }
+    }
+}
