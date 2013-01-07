f90568744bd86ee88369f81c94d4bba971d01dd0
hoangitk_cp <SND\hoangitk_cp>
2013-01-07 19:19:25 +0000

    add ChangAndNotify

--- ExtendMethodHelper.cs
+++ ExtendMethodHelper.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Drawing;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 using System.Windows.Forms;
 
@@ -49,6 +52,11 @@ namespace TimeSheetControl
             return sb.ToString();
         }
 
+        /// <summary>
+        /// Extend method to get cell bound
+        /// </summary>
+        /// <param name="cell"></param>
+        /// <returns></returns>
         public static Rectangle GetCellBoundRectangle(this DataGridViewCell cell)
         {
             var gridView = cell.DataGridView;
@@ -68,6 +76,14 @@ namespace TimeSheetControl
             return Rectangle.Empty;
         }
 
+        /// <summary>
+        /// Check a point (mouse) is in a corner of the rectangle or not
+        /// </summary>
+        /// <param name="rect"></param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="aligment"></param>
+        /// <returns></returns>
         public static bool CheckPointInCorner(this Rectangle rect, int x, int y, ContentAlignment aligment)
         {
             int deltaX = rect.Width / 3;
@@ -105,6 +121,47 @@ namespace TimeSheetControl
                         && (y0) < y && y <= (y0 + deltaY);
                 default:
                     return false;
+            }
+        }
+
+        /// <summary>
+        /// Raise the PropertyChangedEvent if value is changed
+        /// </summary>
+        /// <param name="changedEventHandler"></param>
+        /// <param name="expression"></param>
+        public static void ChangeAndNotify<T>(this PropertyChangedEventHandler changedEventHandler, ref T field, T value, Expression<Func<T>> expression)
+        {
+            field = value;
+
+            if (!EqualityComparer<T>.Default.Equals(field, value))
+            {
+                if (changedEventHandler == null)
+                    return;
+
+                var lambda = expression as LambdaExpression;
+
+                MemberExpression memberExp;
+
+                if (lambda.Body is UnaryExpression)
+                {
+                    var unaryExp = lambda.Body as UnaryExpression;
+                    memberExp = unaryExp.Operand as MemberExpression;
+                }
+                else
+                {
+                    memberExp = lambda.Body as MemberExpression;
+                }
+
+                var constantExp = memberExp.Expression as ConstantExpression;
+                var propertyName = memberExp.Member.Name;// as PropertyInfo;
+
+                foreach (var dele in changedEventHandler.GetInvocationList())
+                {
+                    dele.DynamicInvoke(new object[] {
+                        constantExp.Value,
+                        new PropertyChangedEventArgs(propertyName)} 
+                    );
+                }
             }
         }
     }
