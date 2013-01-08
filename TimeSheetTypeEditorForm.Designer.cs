6a2459efb3bef2578451ecb5c43be8d65d267cd6
hoangitk_cp <SND\hoangitk_cp>
2013-01-08 18:04:01 +0000

    TimeSheetEditorForm

---
+++ TimeSheetTypeEditorForm.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace TimeSheetControl
+{
+    partial class TimeSheetTypeEditorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label catalogLabel;
+            System.Windows.Forms.Label codeLabel;
+            System.Windows.Forms.Label idLabel;
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.catalogComboBox = new System.Windows.Forms.ComboBox();
+            this.codeTextBox = new System.Windows.Forms.TextBox();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.timeSheetTypeBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.btnUpdate = new System.Windows.Forms.Button();
+            catalogLabel = new System.Windows.Forms.Label();
+            codeLabel = new System.Windows.Forms.Label();
+            idLabel = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.timeSheetTypeBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 75F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(idLabel, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.idTextBox, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(codeLabel, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.codeTextBox, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(catalogLabel, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.catalogComboBox, 1, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(205, 74);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // catalogLabel
+            // 
+            catalogLabel.AutoSize = true;
+            catalogLabel.Location = new System.Drawing.Point(3, 48);
+            catalogLabel.Name = "catalogLabel";
+            catalogLabel.Size = new System.Drawing.Size(46, 13);
+            catalogLabel.TabIndex = 0;
+            catalogLabel.Text = "Catalog:";
+            // 
+            // catalogComboBox
+            // 
+            this.catalogComboBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.timeSheetTypeBindingSource, "Catalog", true));
+            this.catalogComboBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.catalogComboBox.FormattingEnabled = true;
+            this.catalogComboBox.Location = new System.Drawing.Point(78, 51);
+            this.catalogComboBox.Name = "catalogComboBox";
+            this.catalogComboBox.Size = new System.Drawing.Size(124, 21);
+            this.catalogComboBox.TabIndex = 1;
+            // 
+            // codeLabel
+            // 
+            codeLabel.AutoSize = true;
+            codeLabel.Location = new System.Drawing.Point(3, 24);
+            codeLabel.Name = "codeLabel";
+            codeLabel.Size = new System.Drawing.Size(35, 13);
+            codeLabel.TabIndex = 2;
+            codeLabel.Text = "Code:";
+            // 
+            // codeTextBox
+            // 
+            this.codeTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.timeSheetTypeBindingSource, "Code", true));
+            this.codeTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.codeTextBox.Location = new System.Drawing.Point(78, 27);
+            this.codeTextBox.Name = "codeTextBox";
+            this.codeTextBox.Size = new System.Drawing.Size(124, 20);
+            this.codeTextBox.TabIndex = 3;
+            // 
+            // idLabel
+            // 
+            idLabel.AutoSize = true;
+            idLabel.Location = new System.Drawing.Point(3, 0);
+            idLabel.Name = "idLabel";
+            idLabel.Size = new System.Drawing.Size(19, 13);
+            idLabel.TabIndex = 4;
+            idLabel.Text = "Id:";
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.timeSheetTypeBindingSource, "Id", true));
+            this.idTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.idTextBox.Location = new System.Drawing.Point(78, 3);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(124, 20);
+            this.idTextBox.TabIndex = 5;
+            // 
+            // timeSheetTypeBindingSource
+            // 
+            this.timeSheetTypeBindingSource.DataSource = typeof(TimeSheetControl.TimeSheetType);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnUpdate.Location = new System.Drawing.Point(3, 77);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(205, 23);
+            this.btnUpdate.TabIndex = 5;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // TimeSheetTypeEditorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(211, 103);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.btnUpdate);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "TimeSheetTypeEditorForm";
+            this.Padding = new System.Windows.Forms.Padding(3);
+            this.Text = "TimeSheetTypeEditorForm";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.timeSheetTypeBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.BindingSource timeSheetTypeBindingSource;
+        private System.Windows.Forms.TextBox codeTextBox;
+        private System.Windows.Forms.ComboBox catalogComboBox;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
---
+++ TimeSheetTypeEditorForm.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TimeSheetControl
+{
+    public partial class TimeSheetTypeEditorForm : Form, IDataGridViewEditingControl
+    {
+        private TimeSheetType _value;
+
+        public TimeSheetType Value
+        {
+            get { return _value; }
+            set { _value = value; }
+        }       
+
+        DataGridView _dataGridView;
+        int _rowIndex;
+        bool _valueChanged = false;
+
+        public event EventHandler ValueUpdated;
+        protected virtual void OnValueUpdated()
+        {
+            EventHandler handler = ValueUpdated;
+            if (handler != null)
+                handler(this, EventArgs.Empty);
+        }
+
+        public TimeSheetTypeEditorForm(TimeSheetType tsType)
+        {
+            InitializeComponent();
+
+            var catalogDataSource = ExtendMethodHelper.EnumToList<TimeSheetCatalog>();
+            this.catalogComboBox.DataSource = catalogDataSource;
+            this.catalogComboBox.DisplayMember = "Key";
+            this.catalogComboBox.ValueMember = "Value";
+
+            this.Value = tsType;
+
+            this.Load += (s, e) =>
+            {
+                this.timeSheetTypeBindingSource.DataSource = this.Value;
+            };
+
+            this.btnUpdate.Click += (s, e) =>
+            {
+                ClosePopup();
+
+                _valueChanged = true;
+                this.EditingControlDataGridView.NotifyCurrentCellDirty(true);
+
+                OnValueUpdated();
+            };
+        }
+
+        public TimeSheetTypeEditorForm() : this(new TimeSheetType())
+        {
+
+        }
+
+        #region Unmanaged Code
+
+        [DllImport("user32", CharSet = CharSet.Auto)]
+
+        private extern static int SendMessage(IntPtr handle, int msg, int wParam, IntPtr lParam);
+
+        [DllImport("user32", CharSet = CharSet.Auto)]
+
+        private extern static int PostMessage(IntPtr handle, int msg, int wParam, IntPtr lParam);
+
+        private const int WM_ACTIVATE = 0x006;
+
+        private const int WM_ACTIVATEAPP = 0x01C;
+
+        private const int WM_NCACTIVATE = 0x086;
+
+        [DllImport("user32")]
+
+        private extern static void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+
+        private const int KEYEVENTF_KEYUP = 0x0002;
+
+        #endregion
+
+        private bool popupShowing = false;
+
+        public bool PopupShowing
+        {
+            get
+            {
+                return popupShowing;
+            }
+
+            set
+            {
+                popupShowing = value;
+            }
+        }
+
+        public Form DataGridViewParentForm
+        {
+            get
+            {
+                return _dataGridView.TopLevelControl as Form;
+            }
+        }
+
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            if (this.popupShowing)
+            {
+                // check for WM_ACTIVATE and WM_NCACTIVATE
+                if (m.Msg == WM_NCACTIVATE)
+                {
+                    // Check if the title bar will active:
+                    if (((int)m.WParam) == 1)
+                    {
+                        // If so reactivate it.
+                        SendMessage(DataGridViewParentForm.Handle, WM_NCACTIVATE, 1, IntPtr.Zero);
+
+                        // Note it's no good to try and consume this message;
+                        // if you try to do that you'll end up with windows
+                        // that don't respond.
+                    }
+                }
+
+                else if (m.Msg == WM_ACTIVATEAPP)
+                {
+                    // Check if the application is being deactivated.
+                    if ((int)m.WParam == 0)
+                    {
+                        // It is so cancel the popup:
+                        ClosePopup();
+
+                        // And put the title bar into the inactive state:
+                        PostMessage(DataGridViewParentForm.Handle, WM_NCACTIVATE, 0, IntPtr.Zero);
+                    }
+                }
+            }
+        }
+
+        public void ClosePopup()
+        {
+            if (this.popupShowing)
+            {
+                this.popupShowing = false;
+
+                this.Hide();
+            }
+        }
+
+        protected override bool ProcessDialogKey(Keys keyData)
+        {
+            Keys key = (keyData & Keys.KeyCode);
+
+            if (key == Keys.Tab)
+            {
+                Point pt = _dataGridView.CurrentCellAddress;
+
+                //if this column is not the last column
+                if ((keyData & Keys.Shift) == Keys.Shift)
+                {
+                    if (pt.X > 0)
+                    {
+                        _dataGridView.CurrentCell = _dataGridView[pt.X - 1, pt.Y];
+                    }
+                }
+                else
+                {
+                    if (pt.X < _dataGridView.Columns.Count - 1)
+                    {
+                        _dataGridView.CurrentCell = _dataGridView[pt.X + 1, pt.Y];
+                    }
+                }
+            }
+
+            return base.ProcessDialogKey(keyData);
+        }
+
+        #region IDataGridViewEditingControl
+        public void ApplyCellStyleToEditingControl(DataGridViewCellStyle dataGridViewCellStyle)
+        {
+
+        }
+
+        public DataGridView EditingControlDataGridView
+        {
+            get
+            {
+                return _dataGridView;
+            }
+            set
+            {
+                _dataGridView = value;
+            }
+        }
+
+        public object EditingControlFormattedValue
+        {
+            get
+            {
+                return this.Value.ToString();
+            }
+            set
+            {                
+                //this.Value = value as TimeSheetType;
+            }
+        }
+
+        public int EditingControlRowIndex
+        {
+            get
+            {
+                return _rowIndex;
+            }
+            set
+            {
+                _rowIndex = value;
+            }
+        }
+
+        public bool EditingControlValueChanged
+        {
+            get
+            {
+                return _valueChanged;
+            }
+            set
+            {
+                _valueChanged = value;
+            }
+        }
+
+        public bool EditingControlWantsInputKey(Keys keyData, bool dataGridViewWantsInputKey)
+        {
+            return !dataGridViewWantsInputKey;
+        }
+
+        public Cursor EditingPanelCursor
+        {
+            get { return base.Cursor; }
+        }
+
+        public object GetEditingControlFormattedValue(DataGridViewDataErrorContexts context)
+        {
+            return this.EditingControlFormattedValue;
+        }
+
+        public void PrepareEditingControlForEdit(bool selectAll)
+        {
+
+        }
+
+        public bool RepositionEditingControlOnValueChange
+        {
+            get { return false; }
+        }
+        #endregion
+    }
+}
