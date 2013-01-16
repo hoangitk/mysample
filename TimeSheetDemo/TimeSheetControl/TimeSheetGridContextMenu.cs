961d473194cff76e15e8f54fd23ac978bca12a06
hoangitk_cp <SND\hoangitk_cp>
2013-01-16 03:43:12 +0000

    Finish demo

--- TimeSheetDemo/TimeSheetControl/TimeSheetGridContextMenu.cs
+++ TimeSheetDemo/TimeSheetControl/TimeSheetGridContextMenu.cs
@@ -7,40 +7,43 @@ namespace TimeSheetControl
 {
     public class TimeSheetGridContextMenu : MyContextMenu
     {
-        public MyMenuItem Copy             { get; private set; }
-        public MyMenuItem Paste            { get; private set; }
-        public MyMenuItem AssignShift      { get; private set; }
-        public MyMenuItem DeleteShift      { get; private set; }
-        public MyMenuItem AssignFullDayOff { get; private set; }
-        public MyMenuItem AssignHalfDayOff { get; private set; }
-        public MyMenuItem DeleteDayOff     { get; private set; }
-        public MyMenuItem AssignFullLeave  { get; private set; }
-        public MyMenuItem AssignHalfLeave  { get; private set; }
-        public MyMenuItem DeleteLeave      { get; private set; }
+        public MyMenuItem MenuItemCopy               { get; private set; }
+        public MyMenuItem MenuItemPaste              { get; private set; }
+        public MyMenuItem MenuItemPasteNormal        { get; private set; }
+        public MyMenuItem MenuItemPasteSelectedCells { get; private set; }
+        public MyMenuItem MenuItemAssignShift        { get; private set; }        
+        public MyMenuItem MenuItemAssignFullDayOff   { get; private set; }
+        public MyMenuItem MenuItemAssignHalfDayOff   { get; private set; }
+        public MyMenuItem MenuItemAssignFullLeave    { get; private set; }
+        public MyMenuItem MenuItemAssignHalfLeave    { get; private set; }
+        public MyMenuItem MenuItemDelete             { get; private set; }        
 
         public TimeSheetGridContextMenu()
         {
-            this.Copy             = new MyMenuItem("Copy"               );
-            this.Paste            = new MyMenuItem("Paste"              );
-            this.AssignShift      = new MyMenuItem("Assign Shift"       );
-            this.DeleteShift      = new MyMenuItem("Delete Shift"       );
-            this.AssignFullDayOff = new MyMenuItem("Assign Full Day Off");
-            this.AssignHalfDayOff = new MyMenuItem("Assign Half Day Off");
-            this.DeleteDayOff     = new MyMenuItem("Delete Day Off"     );
-            this.AssignFullLeave  = new MyMenuItem("Assign Full Leave"  );
-            this.AssignHalfLeave  = new MyMenuItem("Assign Half Leave"  );
-            this.DeleteLeave      = new MyMenuItem("Delete Leave"       );
+            this.MenuItemCopy               = new MyMenuItem("Copy"               );
+            this.MenuItemPaste              = new MyMenuItem("Paste"              );
+            this.MenuItemPasteNormal        = new MyMenuItem("Normal"             );
+            this.MenuItemPasteSelectedCells = new MyMenuItem("Into selected cell(s)");
+            this.MenuItemAssignShift        = new MyMenuItem("Assign Shift"       );            
+            this.MenuItemAssignFullDayOff   = new MyMenuItem("Assign Full Day Off");
+            this.MenuItemAssignHalfDayOff   = new MyMenuItem("Assign Half Day Off");            
+            this.MenuItemAssignFullLeave    = new MyMenuItem("Assign Full Leave"  );
+            this.MenuItemAssignHalfLeave    = new MyMenuItem("Assign Half Leave"  );
+            this.MenuItemDelete             = new MyMenuItem("Delete"             );            
 
-            this.AddMenuItem(this.Copy             );
-            this.AddMenuItem(this.Paste            );
-            this.AddMenuItem(this.AssignShift      );
-            this.AddMenuItem(this.DeleteShift      );
-            this.AddMenuItem(this.AssignFullDayOff );
-            this.AddMenuItem(this.AssignHalfDayOff );
-            this.AddMenuItem(this.DeleteDayOff     );
-            this.AddMenuItem(this.AssignFullLeave  );
-            this.AddMenuItem(this.AssignHalfLeave  );
-            this.AddMenuItem(this.DeleteLeave      );
+            this.AddMenuItem(this.MenuItemCopy             );
+            this.AddMenuItem(this.MenuItemPaste 
+                                    .AddChild(this.MenuItemPasteNormal)
+                                    .AddChild(this.MenuItemPasteSelectedCells)
+                            );
+            this.AddSeperator();
+            this.AddMenuItem(this.MenuItemAssignShift      );
+            this.AddMenuItem(this.MenuItemAssignFullDayOff );
+            this.AddMenuItem(this.MenuItemAssignHalfDayOff );
+            this.AddMenuItem(this.MenuItemAssignFullLeave  );
+            this.AddMenuItem(this.MenuItemAssignHalfLeave  );
+            this.AddSeperator();
+            this.AddMenuItem(this.MenuItemDelete           );
         }
     }
 }
--- TimeSheetDemo/TimeSheetDemo/MainForm.cs
+++ TimeSheetDemo/TimeSheetDemo/MainForm.cs
@@ -15,6 +15,7 @@ using TimeSheetControl;
 using Cadena.WinForms;
 using System.Diagnostics;
 using System.Collections;
+using System.Linq;
 
 namespace TimeSheetDemo
 {
@@ -25,9 +26,7 @@ namespace TimeSheetDemo
 	{
 		private Random rand = new Random();
 
-        private BindingList<TimeSheetItem> _timeSheetItems;
-
-        private ArrayList _styleList;
+        private BindingList<TimeSheetItem> _timeSheetItems;        
 
         TimeSheetGridContextMenu ctxmnuGridView;
 
@@ -67,11 +66,46 @@ namespace TimeSheetDemo
             }
 
             // Define context menu for grid
-            #region Sample define context menu            
-            this.ctxmnuGridView = new TimeSheetGridContextMenu();            
+            #region Sample define context menu
+            
+            this.ctxmnuGridView = new TimeSheetGridContextMenu();
+            InitContextMenu();
+
             #endregion
 
 		}
+
+        private void InitContextMenu()
+        {
+            this.ctxmnuGridView.MenuItemCopy
+                .SetCommand((s, me) => this.tsGridView.CopyToClipBoard())
+                .SetShortcutKeys(Keys.Control | Keys.C);
+            this.ctxmnuGridView.MenuItemPasteNormal
+                .SetCommand((s, me) => this.tsGridView.PasteFromClipBoard())
+                .SetShortcutKeys(Keys.Control | Keys.V);
+            this.ctxmnuGridView.MenuItemPasteSelectedCells
+                .SetCommand((s, me) => this.tsGridView.PasteFromClipBoard(false))
+                .SetShortcutKeys(Keys.Control | Keys.Shift | Keys.V);            
+            this.ctxmnuGridView.MenuItemAssignFullDayOff
+                .SetCommand((s, me) => MessageBox.Show(s.ToString()));
+            this.ctxmnuGridView.MenuItemAssignFullLeave
+                .SetCommand((s, me) => MessageBox.Show(s.ToString()));
+            this.ctxmnuGridView.MenuItemAssignHalfDayOff
+                .SetCommand((s, me) => MessageBox.Show(s.ToString()));
+            this.ctxmnuGridView.MenuItemAssignHalfLeave
+                .SetCommand((s, me) => MessageBox.Show(s.ToString()));
+            this.ctxmnuGridView.MenuItemDelete
+                .SetCommand((s, me) => MessageBox.Show(s.ToString()));
+
+            // Sample: Assign Shift
+            var shifts = SampleData.Default.SampleTimeSheetTypeList.Where(t => t.Catalog == TimeSheetCatalog.Shift);
+            foreach (var tsShift in shifts)
+            {
+                this.ctxmnuGridView.MenuItemAssignShift
+                    .AddChild(new MyMenuItem(tsShift.Code.ToString())
+                        .SetCommand((s, me) => MessageBox.Show(s.ToString())));
+            }
+        }
 
         private void TimeSheetGridView_MouseUp(object sender, MouseEventArgs e)
         {
@@ -81,6 +115,46 @@ namespace TimeSheetDemo
                 if (hitInfo.ColumnIndex >= this.tsGridView.ColumnHeaderCount
                     && hitInfo.RowIndex >= 0)
                 {
+                    this.ctxmnuGridView.MenuItemDelete.ClearChildren();
+
+                    // Generate Delete command for ContextMenu
+                    // Assume: Only accept 1 cell same time
+                    if (this.tsGridView.SelectedCells.Count == 1)
+                    {
+                        var tsDay = this.tsGridView.SelectedCells[0].Value as TimeSheetDay;
+                        if (tsDay != null)
+                        {
+                            if (tsDay.ShiftItems != null && tsDay.ShiftItems.Count > 0)
+                            {                                
+                                foreach (TimeSheetRecord tsRecord in tsDay.ShiftItems)
+                                {
+                                    this.ctxmnuGridView.MenuItemDelete
+                                        .AddChild(new MyMenuItem(string.Format("{0} [{1}-{2}]", 
+                                                                    tsRecord.TimeSheetType,
+                                                                    tsRecord.FromTime.ToString("HH:mm"),
+                                                                    tsRecord.ToTime.ToString("HH:mm")))
+                                            .SetCommand((s, me) => MessageBox.Show(s.ToString())));
+                                }
+                            }// Shift
+
+                            if (tsDay.LeaveItems != null && tsDay.LeaveItems.Count > 0)
+                            {
+                                this.ctxmnuGridView.MenuItemDelete.AddSeperator();
+
+                                foreach (TimeSheetRecord tsRecord in tsDay.LeaveItems)
+                                {
+                                    this.ctxmnuGridView.MenuItemDelete
+                                        .AddChild(new MyMenuItem(string.Format("{0} [{1}-{2}]",
+                                                                    tsRecord.TimeSheetType,
+                                                                    tsRecord.FromTime.ToString("HH:mm"),
+                                                                    tsRecord.ToTime.ToString("HH:mm")))
+                                            .SetCommand((s, me) => MessageBox.Show(s.ToString()))); 
+                                }
+                            }// Leave                            
+                        }
+                    }
+
+                    // Show ContextMenu
                     this.ctxmnuGridView.Show(this.tsGridView, e.X, e.Y);
                 }
             }
